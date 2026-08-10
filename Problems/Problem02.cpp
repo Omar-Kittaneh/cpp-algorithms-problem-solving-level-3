4c113717355c561652a7b92d3550c6c480579163
Omar Kittaneh <omar.kitt7@gmail.com>
2026-08-10 19:02:05 +0300

    Solve Problem 02

--- Problems/Problem02.cpp
+++ Problems/Problem02.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <iomanip>
 using namespace std;
-int RandomNumber(int From, int To)
-{
+int RandomNumber(int From, int To) {
     int randNum = rand() % (To - From + 1) + From;
     return randNum;
-}void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
+}
+
+void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
 {
     for (short i = 0; i < Rows; i++)
     {
@@ -16,6 +17,23 @@ int RandomNumber(int From, int To)
         }
     }
 }
+
+int SumRows(int arr[3][3] , short Rows, short Cols) {
+    int sum = 0;
+    for (short i = 0; i < Cols; i++) {
+        sum += arr[Rows][i];
+    }
+    return sum;
+}
+
+void PrintEachRowSum(int arr[3][3], short Rows, short Cols)
+{
+    for (short i = 0; i < Rows; i++)
+    {
+        printf("Row %d sum = %d \n", i + 1 , SumRows(arr, i, Cols));
+    }
+}
+
 void PrintMatrix(int arr[3][3], short Rows, short Cols)
 {
     for (short i = 0; i < Rows; i++)
@@ -27,11 +45,20 @@ void PrintMatrix(int arr[3][3], short Rows, short Cols)
         cout << "\n";
     }
 }
-int main() {
+ int main() {
     srand((unsigned)time(NULL));
+
     int arr[3][3];
+
     FillMatrixWithRandomNumbers(arr, 3, 3);
+
     cout << "\n The following is a 3x3 random matrix:\n";
+
     PrintMatrix(arr, 3, 3);
- return 0;
-}
+
+    cout << "\nThe following are the sum of each row in the matrix:\n";
+
+    PrintEachRowSum(arr, 3, 3);
+
+    return 0;
+ }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+using namespace std;
 int main() {
-    std::cout << "Hello World!";
+int arr [3][3] = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9},
+};
+    cout << arr [0][0] << " " << arr [0][1] << " " << arr [0][2] << endl;
     return 0;
 }
